60bd290021835222d24921140503abe90e00340e
Danny Nguyen <danny.duc@gmail.com>
2013-07-08 18:16:31 -0700

    add renders for bullets and headings

--- app/scripts/slides.js
+++ app/scripts/slides.js
@@ -1,4 +1,8 @@
 window.slides = [
     { title: 'My Presentiation' },
+    {
+        title: 'Why This Matters',
+        bullets: ['Point One', 'Point Two', 'Point Three']
+    },
     { image: 'http://placekitten.com/600/300' }
 ];
