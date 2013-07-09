3fa9492278aeadba9e05ce1ea9d19332dc0e3754
Danny Nguyen <danny.duc@gmail.com>
2013-07-08 17:47:16 -0700

    ...

--- app/scripts/models/slide.js
+++ app/scripts/models/slide.js
@@ -2,7 +2,27 @@ define(['backbone'], function(Backbone) {
     var Slide = Backbone.Model.extend({
         defaults: {
             type: 'note',
-            title: ''
+            title: '',
+            size: 'normal'
+        },
+
+        initialize: function() {
+            this.setFontSize();
+        },
+
+        setFontSize: function() {
+            var length = this.get('title').length;
+            var size;
+
+            if (length >= 200) {
+                size = 'x-large';
+            } else if (length >= 100) {
+                size = 'large';
+            } else {
+                size = 'normal';
+            }
+
+            this.set('size', size);
         }
     });
 
---
+++ app/scripts/slides.js
@@ -0,0 +1,6 @@
+window.slides = [
+    { title: 'My Presentiation' },
+    { title: 'Hello, my name is Jeffrey.' },
+    { title: 'I work for Envato' },
+    { title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad' }
+];
