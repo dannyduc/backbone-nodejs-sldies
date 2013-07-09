db6c93effd0ac58432f0be337e4ed1585605068c
Danny Nguyen <danny.duc@gmail.com>
2013-07-08 17:57:51 -0700

    add image render option

--- app/scripts/slides.js
+++ app/scripts/slides.js
@@ -1,6 +1,4 @@
 window.slides = [
     { title: 'My Presentiation' },
-    { title: 'Hello, my name is Jeffrey.' },
-    { title: 'I work for Envato' },
-    { title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad' }
+    { image: 'http://placekitten.com/600/300' }
 ];
